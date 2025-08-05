e0e812dae6bfc30581fb7910a32bf9f0acc77ce8
uttam9721 <uttammaurya377@gmail.com>
2025-08-05 11:29:04 +0530

    done

---
+++ JavaScript/index.js
@@ -0,0 +1,26 @@
+// console.log("Execution");
+// console.warn("Warn");
+// console.error("Warm");
+
+// var 
+// var a = 10; declare and initialise
+// a=20; // re initialisation
+// var a = 40; // re declaration
+// console.log(a);
+
+
+
+// let 
+// let b = 10;   //declare and initialise
+// b=20;  // re initialisation
+// let b = 30;   // we can not re declare the value
+// console.log(b);
+
+
+// const 
+// const c =10;  //declare and initialise
+// c = 20;  // we can not re initialise
+// const c = true;  // can not allow re declare.
+// console.log(c);
+
+
