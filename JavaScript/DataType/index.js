dffd18173087a03d080469bff1d03d6e02ddf815
uttam9721 <uttammaurya377@gmail.com>
2025-08-07 11:46:47 +0530

    done

---
+++ JavaScript/DataType/index.js
@@ -0,0 +1,37 @@
+
+let str = "";
+console.log(typeof str);
+
+let bool = true;
+bool = false;
+bool =1;
+
+console.log(typeof bool);
+console.log(true+true);
+console.log(true+true+true);
+console.log(true+false);
+let str1 ="hii";
+str1="";
+str1=" ";
+str1="null";
+console.log(typeof str1);
+
+
+console.log(true+false);
+console.log(typeof Object);
+console.log(typeof undefined);
+console.log(null + null);
+console.log(typeof null);
+
+let m;
+console.log(typeof m);
+
+// Number==========
+let a =10;
+a=.200;
+a=-10;
+a=-.20;
+console.log(typeof a);
+
+// undefined
+
