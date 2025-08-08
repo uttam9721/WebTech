2134420eb91a1930e74419a297f919d8b94508bf
uttam9721 <uttammaurya377@gmail.com>
2025-08-08 14:47:48 +0530

    d

--- JavaScript/DataType/index.js
+++ JavaScript/DataType/index.js
@@ -1,37 +1,51 @@
 
-let str = "";
-console.log(typeof str);
+// let str = "";
+// console.log(typeof str);
 
-let bool = true;
-bool = false;
-bool =1;
+// let bool = true;
+// bool = false;
+// bool =1;
 
-console.log(typeof bool);
-console.log(true+true);
-console.log(true+true+true);
-console.log(true+false);
-let str1 ="hii";
-str1="";
-str1=" ";
-str1="null";
-console.log(typeof str1);
+// console.log(typeof bool);
+// console.log(true+true);
+// console.log(true+true+true);
+// console.log(true+false);
+// let str1 ="hii";
+// str1="";
+// str1=" ";
+// str1="null";
+// console.log(typeof str1);
 
 
-console.log(true+false);
-console.log(typeof Object);
-console.log(typeof undefined);
-console.log(null + null);
-console.log(typeof null);
+// console.log(true+false);
+// console.log(typeof Object);
+// console.log(typeof undefined);
+// console.log(null + null);
+// console.log(typeof null);
 
-let m;
-console.log(typeof m);
+// let m;
+// console.log(typeof m);
 
-// Number==========
-let a =10;
-a=.200;
-a=-10;
-a=-.20;
-console.log(typeof a);
+// // Number==========
+// let a =10;
+// a=.200;
+// a=-10;
+// a=-.20;
+// console.log(typeof a);
 
-// undefined
+// // undefined
 
+
+
+// let a =20;
+// let b=10,c=30;
+// let d, e=40;
+// d=e+a-b;
+// e=d+(d-b)*a;
+// b=a-e;
+// a=e+c;
+// c=a-d;
+// console.log(a,b,c,d,e);
+
+
+let a=
