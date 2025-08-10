1f5df8e73f904cc28bfa0534a67c06e1d6766fdb
uttam9721 <uttammaurya377@gmail.com>
2025-08-10 11:30:47 +0530

    done

---
+++ JavaScript/Loop/index.js
@@ -0,0 +1,7 @@
+let sum =0;
+for(let i=-50;i<=50;i++){
+    if(i%2==0 && i>=0){
+        sum=sum+i;
+    }
+}
+console.log(`this is total output ${sum}`);
---
+++ JavaScript/controlStatement/index.js
@@ -0,0 +1,18 @@
+let trafficLight = "green";
+
+switch (trafficLight) {
+  case "red":
+    console.log("Stop!");
+    break;
+
+  case "yellow":
+    console.log("Get ready!");
+    break;
+
+  case "green":
+    console.log("Go!");
+    break;
+
+  default:
+    console.log("Invalid signal!");
+}
