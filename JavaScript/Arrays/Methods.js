24f6426374cf504f55257e7e260c95e3f07bf0ad
uttam9721 <uttammaurya377@gmail.com>
2025-08-22 10:11:46 +0530

    done

---
+++ JavaScript/Arrays/Methods.js
@@ -0,0 +1,28 @@
+// let arr = [1,2,3,4];
+// let ans = arr.map(e=>e*2);
+// console.log(ans);
+
+// let ans1 =arr.filter(e=>e%2==0);
+// console.log(ans1);
+
+
+// let red= arr.reduce(e,val,0);
+// console.log(red);
+
+// let arr1 = [5,6,7,8,9];
+// // console.log(...arr,...arr1);
+// let ans =arr.concat(arr1);
+// console.log(ans);
+
+
+// let fil = [0,1,2,3,4,5,undefined,null ,"hii",""];
+// let ans=fil.filter(ele=>ele);
+// console.log(ans);
+
+
+if(false){
+    let a =10;
+    console.log(a);
+    
+}
+// console.log(a);
