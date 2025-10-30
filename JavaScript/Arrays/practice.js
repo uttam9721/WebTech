9985a2286d461b99acd35de48850270309439f86
uttam9721 <uttammaurya377@gmail.com>
2025-10-30 22:55:54 +0530

    practice

--- JavaScript/Arrays/practice.js
+++ JavaScript/Arrays/practice.js
@@ -1,3 +1,103 @@
-const prompt = require('prompt-async')();
-console.log(prompt);
+// import prompt from ('prompt-async')();
+
+// let arr = [10,20,30,40,50,60];
+// let sum =0;
+// for(let i=0;i<arr.length;i++){
+//     sum=sum+arr[i];
+// }
+// console.log(sum);
+
+
+
+// Find max Element from an array 
+// let arr = [10,20,30,40,50,60];
+// let max=arr[0];
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>max){
+//         max=arr[i];
+//     }
+// }
+// console.log(max);
+
+
+
+// Find min element from an array
+// let arr = [10,20,30,40,50,60];
+// let min=arr[0];
+// for(let i=0;i<arr.length;i++){
+//     if (arr[i]<min) {
+//         min=arr[i]
+        
+//     }
+// }
+// console.log(min);
+
+
+
+// Find Second minimum
+// let arr = [10,20,30,40,50,60,90,120];
+// let max=-Infinity;
+// let smax=-Infinity;
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>max){
+//         smax=max;
+//         max=arr[i];
+//     }else if (arr[i]>smax && max>arr[i]) {
+//         smax=arr[i];
+        
+//     }
+// }
+// console.log(smax);
+
+
+
+// reverse An array 
+// let arr = [10,20,30,40,50,60];
+// let i=0;
+// let j=arr.length-1;
+// while(i<=j){
+//     let temp=arr[i];
+//     arr[i]=arr[j];
+//     arr[j]=temp;
+//     i++
+//     j--
+// }
+// console.log(arr);
+
+
+
+// move zero at the start of the index 
+// let arr=[0,1,1,1,0,1,0,0,1];
+// let i=0;
+// let j=0;
+// while(i<arr.length){
+//     if (arr[i]==0) {
+//         let temp=arr[i];
+//         arr[i]=arr[j];
+//         arr[j]=temp;
+//         j++        
+//     }
+//     i++
+// }
+// console.log(arr);
+
+
+
+// move zero last 
+// let arr=[0,1,1,1,0,1,0,0,1];
+// let i=0;
+// let j=0;
+// while(i<arr.length){
+//     if (arr[i]!=0) {
+//         let temp=arr[i];
+//         arr[i]=arr[j];
+//         arr[j]=temp;
+//         j++        
+//     }
+//     i++
+// }
+// console.log(arr);
+
+
+
 
